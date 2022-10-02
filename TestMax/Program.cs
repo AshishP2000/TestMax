b11b9c00f83a8d5824ab67a397072b22674c4b5e
Ashish <ashish2000patil@gmail.com>
2022-10-02 20:01:03 +0530

    [Ashish]Added: Welcome to Test Maximum Using Genrics

---
+++ TestMax/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMax
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Welcome to Find Maximum problem using Genrics
+        }
+    }
+}
