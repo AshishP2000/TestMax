107819637567c748a3890f3284c22c1501d8a7ba
Ashish <ashish2000patil@gmail.com>
2022-10-02 20:10:25 +0530

    [Ashish]Added: Find Max Integer UC-1

---
+++ TestMax/FindMax.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMax
+{
+    public class FindMax
+    {
+        public static void MaxInt(int first, int second, int third)
+        {
+            if (first.CompareTo(second) >= 0 && first.CompareTo(third) >= 0)
+            {
+                Console.WriteLine("{0} is Maximum Number between {1} and {2}", first, second, third);
+            }
+            else if (second.CompareTo(first) >= 0 && second.CompareTo(third) >= 0)
+            {
+                Console.WriteLine("{0} is Maximum Number between {1} and {2}", second, first, third);
+            }
+            else
+            {
+                Console.WriteLine("{0} is Maximum Number between {1} and {2}", third, first, second);
+            }
+        }
+    }
+}
--- TestMax/Program.cs
+++ TestMax/Program.cs
@@ -11,6 +11,13 @@ namespace TestMax
         static void Main(string[] args)
         {
             //Welcome to Find Maximum problem using Genrics
+            //UC-1 Given 3 Integers find Maximum
+
+            FindMax.MaxInt(40,30,20);//maximum at first position
+            FindMax.MaxInt(30, 40, 20);//maximum at second position
+            FindMax.MaxInt(30, 20, 40);//maximum at third position
+
+            Console.ReadLine();
         }
     }
 }
