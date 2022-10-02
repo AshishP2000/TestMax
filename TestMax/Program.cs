7d504fc81d75fd3502a8b111fddac339c4bbb97a
Ashish <ashish2000patil@gmail.com>
2022-10-02 20:13:15 +0530

    [Ashish]Added: Find Max float UC-2

--- TestMax/Program.cs
+++ TestMax/Program.cs
@@ -11,11 +11,15 @@ namespace TestMax
         static void Main(string[] args)
         {
             //Welcome to Find Maximum problem using Genrics
-            //UC-1 Given 3 Integers find Maximum
+            //UC-2 Given 3 Floats find Maximum
 
-            FindMax.MaxInt(40,30,20);//maximum at first position
-            FindMax.MaxInt(30, 40, 20);//maximum at second position
-            FindMax.MaxInt(30, 20, 40);//maximum at third position
+            //FindMax.MaxInt(40,30,20);//maximum at first position
+            //FindMax.MaxInt(30, 40, 20);//maximum at second position
+            //FindMax.MaxInt(30, 20, 40);//maximum at third position
+
+            FindMax.MaxFloat(40.40f,30.30f,20.20f);//maximum at first position
+            FindMax.MaxFloat(30.30f, 40.40f, 20.20f);//maximum at second position
+            FindMax.MaxFloat(30.30f, 20.20f, 40.40f);//maximum at third position
 
             Console.ReadLine();
         }
