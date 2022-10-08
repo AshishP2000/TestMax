9e69bc377e25a996a43de916b81d3155de1a5961
Ashish <ashish2000patil@gmail.com>
2022-10-08 17:59:28 +0530

    [Ashish]Added: Find Max Generic Use Options UC-5

--- TestMax/Program.cs
+++ TestMax/Program.cs
@@ -13,10 +13,28 @@ namespace TestMax
             //Welcome to Find Maximum problem using Genrics
             //UC-4 Extend max method 
 
+            Console.WriteLine("1.Maximum Integer\n2.Maximum Float/Double\n3.Maximum String");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    int[] arr = { 112, 34, 2324, 456, 23 };
+                    FindMax<int> findmaxint = new FindMax<int>(arr);
+                    findmaxint.PrintMaxValue();
+                    break;
+                case 2:
+                    double[] arr1 = { 12.12, 34.34, 232.24, 456.45, 233.44 };
+                    FindMax<double> findmaxdouble = new FindMax<double>(arr1);
+                    findmaxdouble.PrintMaxValue();
+                    break;
+                case 3:
+                    string[] arr2 = { "peach", "apple", "banana", "sugercane", "dragonfruit" };
+                    FindMax<string> findmaxstring = new FindMax<string>(arr2);
+                    findmaxstring.PrintMaxValue();
+                    break;
+            }
 
-            int[] arr = {112,34,2324,456,23};
-            FindMax<int> findmax = new FindMax<int>(arr);
-            findmax.PrintMaxValue();
+            
 
             Console.ReadLine();
         }
